615668242d7ada048ca6de9f81be8ca5ba48e2e2
Alli Bateman <allibateman@Allis-MacBook-Pro.local>
2020-04-28 09:45:43 +0200

    Edited the cartridges

---
+++ main.js
@@ -0,0 +1,29 @@
+fetch("https://owldesign.dk/wordpress/wp-json/wp/v2/art_exhibition")
+.then(res => res.json())
+.then(handleData)
+
+function handleData(posts) {
+ console.log(posts);
+posts.forEach(showPost)
+}
+
+function showPost(post){
+    console.log(post)
+    const template = document.querySelector("template").content;
+
+    const copy = template.cloneNode(true);
+
+    copy.querySelector(".id").textContent = post.id;
+
+    copy.querySelector(".post_img").src = post.thumbnail.guid;
+
+    copy.querySelector(".title").textContent = post.name_exhibition;
+
+    copy.querySelector(".artist").textContent = post.name_artist;
+
+    copy.querySelector(".start_date span").textContent = post.starting_date;
+
+    copy.querySelector(".end_date span").textContent = post.ending_date;
+
+    document.querySelector("main").appendChild(copy);
+}
