d2d3c777967db2ff0c8289b8c56188ceacc40a0e
HarveyThePooka404 <57190763+HarveyThePooka404@users.noreply.github.com>
2020-04-30 13:51:01 +0200

    added art spaces

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function handleData(posts) {
 
 function showPost(post) {
     console.log(post)
-    const template = document.querySelector("template").content;
+    const template = document.querySelector(".template_exh").content;
 
     const copy = template.cloneNode(true);
 
@@ -28,5 +28,37 @@ function showPost(post) {
 
     copy.querySelector(".exhibition_place").textContent = post.exhibition_place;
 
-    document.querySelector(".cartridge_wrapper").appendChild(copy);
+    document.querySelector(".cartridge_wrapper.art").appendChild(copy);
+}
+
+// starting same for art spaces
+
+fetch("https://owldesign.dk/wordpress/wp-json/wp/v2/art_space?per_page=3&orderby=date")
+    .then(res => res.json())
+    .then(handleData2)
+
+function handleData2(spaces) {
+    console.log(spaces);
+    spaces.forEach(showSpace)
+
+}
+
+function showSpace(space) {
+    console.log(space);
+    const template = document.querySelector(".template_space").content;
+
+    const copy_space = template.cloneNode(true);
+
+    copy_space.querySelector(".id").textContent = space.id;
+
+    copy_space.querySelector(".space_img").src = space.thumbnail_art_space.guid;
+
+    copy_space.querySelector(".name_space").textContent = space.name_art_space;
+
+    copy_space.querySelector(".address_space").textContent = space.address_art_space;
+
+    copy_space.querySelector(".website_space").textContent = space.website_art_space;
+    copy_space.querySelector(".website_space").href = space.website_art_space;
+
+    document.querySelector(".cartridge_wrapper.space").appendChild(copy_space);
 }
