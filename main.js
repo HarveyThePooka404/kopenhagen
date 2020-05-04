617c78f43add51ee13fad549d9c0886760f4023a
HarveyThePooka404 <57190763+HarveyThePooka404@users.noreply.github.com>
2020-05-04 09:03:57 +0200

    changed post merge with kalendar

--- main.js
+++ main.js
@@ -126,36 +126,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     //then?
 
-    fetch("https://owldesign.dk/wordpress/wp-json/wp/v2/art_exhibition?per_page=3&orderby=date")
-        .then(res => res.json())
-        .then(handleData)
-
-    function handleData(posts) {
-        posts.forEach(showPost)
-
-    }
-
-    function showPost(post) {
-        const template = document.querySelector(".template_exh").content;
-
-        const copy = template.cloneNode(true);
-
-        copy.querySelector(".id").textContent = post.id;
-
-        copy.querySelector(".post_img").src = post.thumbnail.guid;
-
-        copy.querySelector(".title").textContent = post.name_exhibition;
-
-        copy.querySelector(".artist").textContent = post.name_artist;
-
-        copy.querySelector(".start_date").textContent = post.starting_date;
-
-        copy.querySelector(".end_date").textContent = post.ending_date;
-
-        copy.querySelector(".exhibition_place").textContent = post.exhibition_place;
-
-        document.querySelector(".cartridge_wrapper.art").appendChild(copy);
-    }
 
 });
 
